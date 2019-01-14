c4573f7b1ff346f5e88f54f21a060aa094dcb664
石黒太志 <ishigurofutoshi@macbook-pro.local>
2019-01-14 21:29:18 +0900

    taskアプリを製作

--- taskapp/ViewController.swift
+++ taskapp/ViewController.swift
@@ -7,19 +7,117 @@
 //
 
 import UIKit
+import RealmSwift
+import UserNotifications
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate {
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var searchBar: UISearchBar! //追加
+    
+    let realm = try! Realm()
+    
+    var taskArray = try! Realm().objects(Task.self).sorted(byKeyPath: "date", ascending: false)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tableView.delegate = self
+        tableView.dataSource = self
+        searchBar.delegate = self //追加
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return taskArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        let task = taskArray[indexPath.row]
+        
+        cell.textLabel?.text = task.title
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm"
+        
+        let dateString:String = formatter.string(from: task.date)
+        cell.detailTextLabel?.text = dateString
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "cellSegue", sender: nil)
+    }
+    
+    func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCellEditingStyle {
+        return .delete
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete{
+            
+            let task = self.taskArray[indexPath.row]
+            
+            let center = UNUserNotificationCenter.current()
+            center.removePendingNotificationRequests(withIdentifiers: [String(task.id)])
+                
+            try! realm.write{
+                self.realm.delete(task)
+                tableView.deleteRows(at: [indexPath], with: .fade)
+            }
+            
+            center.getPendingNotificationRequests{ (requests: [UNNotificationRequest]) in
+                for request in requests {
+                    print("/----------------")
+                    print(request)
+                    print("----------------/")
+                }
+            }
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?){
+        let inputViewController:InputViewController = segue.destination as! InputViewController
+        
+        if segue.identifier == "cellSegue"{
+            let indexPath = self.tableView.indexPathForSelectedRow
+            inputViewController.task = taskArray[indexPath!.row]
+        }else{
+            let task = Task()
+            task.date = Date()
+            
+            let allTasks = realm.objects(Task.self)
+            if allTasks.count != 0{
+                task.id = allTasks.max(ofProperty: "id")! + 1
+            }
+            inputViewController.task = task
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        tableView.reloadData()
+    }
+    
+    //追加
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.endEditing(true)
+        let match: String = searchBar.text! //searchBarに入力された値
+        
+        if match == ""{
+            taskArray = realm.objects(Task.self).sorted(byKeyPath: "date", ascending: false)
+            tableView.reloadData()
+        }else{
+        taskArray = realm.objects(Task.self).filter("category== %@", match).sorted(byKeyPath: "date", ascending: false) //searchBarに入力された値と一致するcategoryをもつTaskのインスタンスを取得？
+            tableView.reloadData()
+        }
+    }
 
 }
 
